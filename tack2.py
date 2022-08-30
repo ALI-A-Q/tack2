b5cf31064b6396cb3ddea3669efab2ce74524b19
ALI-A-Q <i-k-a4@hotmail.com>
2022-08-30 23:43:54 +0300

    solv cod

---
+++ tack2.py
@@ -0,0 +1,27 @@
+## Q numbre 1
+a=int (input('Enter first numbre'))
+b=int (input('"Enter second number'))
+print(a,b)
+## Q numbre 2
+
+def fun(x,y):
+    funn=(x,y)
+    return (funn)
+print(fun(1,3))
+
+## Q numbre 3
+def defaultvalues(x=0,y=0):
+    default=(x,y)
+    return (default)
+print(defaultvalues())
+
+## Q numbre 4
+def sum(x,y):
+    rsum=(x+y)
+    return (rsum)
+print(sum(15,10))
+
+## Q numbre 5 &6
+sumfunction=lambda x,y:x+y
+print(sumfunction(30,20))
+
